2c0c2aa4a7f64110361f3c680701153d9a3772b2
Alexander <amkatyshev97@gmail.com>
2019-05-30 22:26:02 +0300

    Добавлен метод определения дочерних узлов дерева

--- SRE/TemplateParser/abstracttemplateparser.py
+++ SRE/TemplateParser/abstracttemplateparser.py
@@ -6,6 +6,19 @@ class AbstractTemplateParser:
         __metaclass__ = ABCMeta
         self.__wordModel__ = wordModel
 
+    def __getChildrenByToken__(self, node, conditions={}):
+        result = []
+        conditionKeys = conditions.keys()
+        for child in node.children:
+            isCanAppend = True
+            for key in conditionKeys:
+                if child.token[key] != conditions[key]:
+                    isCanAppend = False
+                    break
+            if isCanAppend:
+                result.append(child)
+        return result
+
     @abstractmethod
     def parse(self, sentence):
         """Разбор предложения для данного шаблона"""
