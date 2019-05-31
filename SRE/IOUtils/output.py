8fba06c1614f1495083a32e3719720516066c29e
Alexander <amkatyshev97@gmail.com>
2019-05-31 17:43:36 +0300

    Исправлен вывод отсутсвующих концептов по типу

--- SRE/IOUtils/output.py
+++ SRE/IOUtils/output.py
@@ -16,13 +16,19 @@ class Output(object):
             with open(self.__filename__, 'a', encoding='utf8') as file:
                 for type, list in data.items():
                     file.write('[' + type + ' relation]\n')
-                    for pair in list:
-                        file.write(' <-> '.join(pair) + '\n')
+                    if list:
+                        for pair in list:
+                            file.write(' <-> '.join(pair) + '\n')
+                    else:
+                        file.write(' -- no concepts --\n')
         elif self.__mode__ == Output.TO_SCREEN:
             for type, list in data.items():
-                print('[' + type + ' relation]\n')
-                for pair in list:
-                    print(' <-> '.join(pair) + '\n')
+                print('[' + type + ' relation]')
+                if list:
+                    for pair in list:
+                        print(' <-> '.join(pair))
+                else:
+                    print(' -- no concepts -- ')
         else:
             raise ValueError('Unknown output mode')
 
