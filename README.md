232d1546074d12446092b647ae5c50eb21525562
Alexander <amkatyshev97@gmail.com>
2019-06-01 15:32:17 +0300

    Созданы README и setup для модуля

---
+++ README.md
@@ -0,0 +1,5 @@
+# SRE (Semantic Relation Extractor)
+
+SRE – модуль для извлечения отношений между концептами предметной области.
+
+[Github Markdown](https://guides.github.com/features/mastering-markdown/).
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SRE",
+    version="1.0.0",
+    author="Alexander Katyshev",
+    author_email="amkatyshev97@gmail.com",
+    description="Semantic Relation Extractor from russian texts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/amkatyshev/sre",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+)
+
