04e4a733cd2703284b2ed5cf545326fdca15d49c
Alexander <amkatyshev97@gmail.com>
2019-06-01 21:50:17 +0300

    Исправлен тест

--- SRE/tests/main.py
+++ SRE/tests/main.py
@@ -5,13 +5,13 @@ from conllu import parse_tree
 import time
 from SRE import SRE
 from SRE.IOUtils import Output
-
+import os
 
 start_time = time.time()
 
 sre = SRE()
-sre.analyze(['программирование'], 'testtext.txt')
-sre.getResultByType('PART-OF', 'кодинг')
+sre.analyze(['шаблон'], os.getcwd() + '/SRE/tests/testtext.txt')
+sre.getResultByType('IS-A', 123)
 
 
 print(time.time() - start_time, 'execution time')
